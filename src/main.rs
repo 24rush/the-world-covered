5ea895ff1c495d4ca2541813c7a2d462881dbd12
24rush <iordache.alin@yahoo.com>
2023-05-19 07:30:58 +0300

    Added CORS support

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,32 @@ use rocket::http::{ContentType, Status};
 #[macro_use]
 extern crate rocket;
 
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::Header;
+use rocket::{Request, Response};
+
+pub struct Cors;
+
+#[rocket::async_trait]
+impl Fairing for Cors {
+    fn info(&self) -> Info {
+        Info {
+            name: "Cross-Origin-Resource-Sharing Fairing",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, PATCH, PUT, DELETE, HEAD, OPTIONS, GET",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
 /*
 fn main() {
     let current_athlete_id = 4399230;
@@ -19,6 +45,11 @@ fn main() {
     app.start_db_pipeline();
 }
 */
+
+#[options("/<_..>")]
+fn all_options() {
+    /* Intentionally left empty */
+}
 
 #[get("/routes/<athlete_id>")]
 async fn routes(athlete_id: &str) -> (Status, (ContentType, String)) {
@@ -40,5 +71,7 @@ async fn routes(athlete_id: &str) -> (Status, (ContentType, String)) {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![routes])
+    rocket::build()
+        .attach(Cors)
+        .mount("/", routes![routes, all_options])
 }
